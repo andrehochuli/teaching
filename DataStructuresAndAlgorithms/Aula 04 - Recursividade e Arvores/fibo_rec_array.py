03130d8b4838d36a992a9c84f0c9f8608fa7c699
andrehochuli <andrehochuli@gmail.com>
2022-09-06 15:26:01 -0300

    Adding Files

--- DataStructuresAndAlgorithms/Aula 04 - Recursividade e Arvores/fibo_rec_array.py
+++ DataStructuresAndAlgorithms/Aula 04 - Recursividade e Arvores/fibo_rec_array.py
@@ -6,14 +6,15 @@ def fibo(n):
     if n == 2:
         arr = fibo(n-1) + [1]
         return arr
-    
+    print('Vai fibo: ',n-1)
     arr = fibo(n-1)
     arr += [sum(arr[-2:])]
-
+    print(arr)
     return arr
 
 #Pos:  1 2 3 4 5 6 7 8  9  
 #Elem: 0 1 1 2 3 5 8 13 21
 
-print(fibo(7))
+
+print(fibo(20))
 
