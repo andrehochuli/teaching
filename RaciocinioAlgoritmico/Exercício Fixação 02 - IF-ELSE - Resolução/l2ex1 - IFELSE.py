16668f58599057879885b8708cbd92fbc594b3ea
Andre <andrehochuli@gmail.com>
2023-02-14 21:16:31 -0300

    Reogarnizando Diretório

--- RaciocinioAlgoritmico/Exercício Fixação 02 - IF-ELSE - Resolução/l2ex1 - IFELSE.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-#Informe se uma pessoa tem o mesmo nome que você
-nome1 = input("Digite um nome: ")
-
-
-if (nome1 == 'Andre'):
-    print("Voce tem o mesmo nome que Andre")
-else:
-    print("Seu nome: ", nome1, " é diferente de Andre")
